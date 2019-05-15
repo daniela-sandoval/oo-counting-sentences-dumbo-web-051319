04e4a617ff6b814e45a38485661c67bf633d10a1
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-05-15 14:46:52 -0400

    defined string class methods

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    new_array = self.split(/\b?\.\s|\?\s|\!\s/) # use www.rubular.com to understand what the argument means (each pipe seperates the period, exclamation point, and question mark) and uses pattern matching.
+    new_array.count
+    end
   end
-end
